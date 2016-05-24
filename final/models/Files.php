4c6a63ea78899691293a2972bb9ff41401e3ce26
user <user@user-PC>
2016-05-24 19:02:33 -0400

    comments

--- final/models/Files.php
+++ final/models/Files.php
@@ -86,6 +86,90 @@ class Files{
         return $rImg;
     }
 
+    function getImageSize(){
+        $imageSize = getimagesize($_FILES[$this->keyName]["tmp_name"]);
+        
+        if (false === $image_info) {
+            throw new RuntimeException('Invalid file format.');
+        }
+        return $imageSize;
+    }
+    
+    function newPicture($image_info , $rImg){
+        $image_width = $image_info[0];
+        $image_height = $image_info[1];
+
+        // Set a maximum height and width
+        $max_width = 300;
+        $max_height = 300;
+
+        $ratio_orig = $image_width / $image_height;
+
+        if ($max_width / $max_height > $ratio_orig) {
+            $max_width = $max_height * $ratio_orig;
+        } else {
+            $max_height = $max_width / $ratio_orig;
+        }
+
+        $image_p = imagecreatetruecolor($max_width, $max_height);
+
+        imagecopyresampled($image_p, $rImg, 0, 0, 0, 0, $max_width, $max_height, $image_width, $image_height);
+
+        $memetop = filter_input(INPUT_POST, 'memetop');
+        $memebottom = filter_input(INPUT_POST, 'memebottom');
+
+        //Font Color (white in this case)
+        $textcolor = imagecolorallocate($image_p, 255, 255, 255);
+        $bgcolor = imagecolorallocate($image_p, 0, 0, 0);
+
+        //y-coordinate of the upper left corner. 
+        $yPos = intval($max_height - 20);
+
+        if (null !== $memetop && strlen($memetop) > 0) {
+            //x-coordinate of center. 
+            $xPosTop = intval(($max_width - 8.5 * strlen($memetop)) / 2);
+            // Set the background
+            imagefilledrectangle($image_p, 0, 0, $max_width, 20, $bgcolor); // top
+            //Writting the picture
+            imagestring($image_p, 5, $xPosTop, 0, $memetop, $textcolor);
+        }
+
+        if (null !== $memebottom && strlen($memebottom) > 0) {
+            //x-coordinate of center. 
+            $xPosBottom = intval(($max_width - 8.5 * strlen($memebottom)) / 2);
+            // Set the background 
+            imagefilledrectangle($image_p, 0, $yPos, $max_width, $max_height, $bgcolor); //bottom
+            //Writting the picture
+            imagestring($image_p, 5, $xPosBottom, $yPos, $memebottom, $textcolor);
+        }
+
+        return $image_p;
+    }
+    
+    function uploadTest($ext, $image_p, $location){
+        switch ($ext) {
+            case "jpg" :
+            case "jpeg" :
+                if (!imagejpeg($image_p, $location)) {
+                    throw new RuntimeException('Failed to move uploaded file.');
+                }
+                break;
+            case "gif" :
+                if (!imagegif($image_p, $location)) {
+                    throw new RuntimeException('Failed to move uploaded file.');
+                }
+                break;
+            case "png" :
+                if (!imagepng($image_p, $location)) {
+                    throw new RuntimeException('Failed to move uploaded file.');
+                }
+                break;
+
+            default :
+                throw new RuntimeException("Error Bad Extention");
+                break;
+        }
+    }
 
 }
 
--- final/views/upload.php
+++ final/views/upload.php
@@ -28,94 +28,11 @@ try {
     $location = sprintf('../uploads/%s.%s', $fileName, $ext);
 
     $rImg = $files->extCheck($ext);
-
-
-
-    $image_info = getimagesize($_FILES["upfile"]["tmp_name"]);
-
-    if (false === $image_info) {
-        throw new RuntimeException('Invalid file format.');
-    }
-
-
-    $image_width = $image_info[0];
-    $image_height = $image_info[1];
-
-
-// Set a maximum height and width
-    $max_width = 300;
-    $max_height = 300;
-
-
-    $ratio_orig = $image_width / $image_height;
-
-    if ($max_width / $max_height > $ratio_orig) {
-        $max_width = $max_height * $ratio_orig;
-    } else {
-        $max_height = $max_width / $ratio_orig;
-    }
-
-    $image_p = imagecreatetruecolor($max_width, $max_height);
-
-    imagecopyresampled($image_p, $rImg, 0, 0, 0, 0, $max_width, $max_height, $image_width, $image_height);
-
-
-
-
-    $memetop = filter_input(INPUT_POST, 'memetop');
-    $memebottom = filter_input(INPUT_POST, 'memebottom');
-    $title = filter_input(INPUT_POST, 'title');
+    $image_info = $files->getImageSize();
+    $image_p = $files->newPicture($image_info, $rImg);
+    $files->uploadTest($ext, $image_p, $location);
     
-//Font Color (white in this case)
-    $textcolor = imagecolorallocate($image_p, 255, 255, 255);
-    $bgcolor = imagecolorallocate($image_p, 0, 0, 0);
-
-//y-coordinate of the upper left corner. 
-    $yPos = intval($max_height - 20);
-
-    if (null !== $memetop && strlen($memetop) > 0) {
-//x-coordinate of center. 
-        $xPosTop = intval(($max_width - 8.5 * strlen($memetop)) / 2);
-// Set the background
-        imagefilledrectangle($image_p, 0, 0, $max_width, 20, $bgcolor); // top
-//Writting the picture
-        imagestring($image_p, 5, $xPosTop, 0, $memetop, $textcolor);
-    }
-
-    if (null !== $memebottom && strlen($memebottom) > 0) {
-//x-coordinate of center. 
-        $xPosBottom = intval(($max_width - 8.5 * strlen($memebottom)) / 2);
-// Set the background 
-        imagefilledrectangle($image_p, 0, $yPos, $max_width, $max_height, $bgcolor); //bottom
-//Writting the picture
-        imagestring($image_p, 5, $xPosBottom, $yPos, $memebottom, $textcolor);
-    }
-
-
-
-    switch ($ext) {
-        case "jpg" :
-        case "jpeg" :
-            if (!imagejpeg($image_p, $location)) {
-                throw new RuntimeException('Failed to move uploaded file.');
-            }
-            break;
-        case "gif" :
-            if (!imagegif($image_p, $location)) {
-                throw new RuntimeException('Failed to move uploaded file.');
-            }
-            break;
-        case "png" :
-            if (!imagepng($image_p, $location)) {
-                throw new RuntimeException('Failed to move uploaded file.');
-            }
-            break;
-
-        default :
-            throw new RuntimeException("Error Bad Extention");
-            break;
-    }
-
+    $title = filter_input(INPUT_POST, 'title');
     $upFileName = $fileName . '.' . $ext;
     $userID = filter_input(INPUT_POST, 'id');
     if(!$db->addMeme($userID, $upFileName, $title)){
@@ -124,15 +41,6 @@ try {
 
     imagedestroy($rImg);
     imagedestroy($image_p);
-    
-   
-
-    /*
-      if ( !move_uploaded_file( $_FILES["upfile"]["tmp_name"], $location) ) {
-        throw new RuntimeException('Failed to move uploaded file.');
-      }
-
-     */
 
     $message = 'File is uploaded successfully.';
 } catch (RuntimeException $e) {
